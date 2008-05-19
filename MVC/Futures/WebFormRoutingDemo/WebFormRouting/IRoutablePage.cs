09b8735f51c3e50f3bab9e9c8ef8939da2ecef6b
SND\AspNetExtensions_cp <SND\AspNetExtensions_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2008-05-19 21:18:21 +0000

    Adding WebFormRouting Demo in MVC/Futures
    git-svn-id: https://aspnet.svn.codeplex.com/svn@6256 e17a0e51-4ae3-4d35-97c3-1a29b211df97

---
+++ MVC/Futures/WebFormRoutingDemo/WebFormRouting/IRoutablePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+
+namespace WebFormRouting
+{
+    /// <summary>
+    /// Pages don't have to implement this interface, but the ones that 
+    /// do will be able to generate outgoing routing URLs.
+    /// </summary>
+    public interface IRoutablePage : IHttpHandler
+    {
+        RequestContext RequestContext { get; set; }
+    }
+}
